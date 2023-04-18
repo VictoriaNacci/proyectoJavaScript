9a51ac691b6745197836af2c0d63d2829ec78bd8
VictoriaNacci <vico.nacci@gmail.com>
2023-04-17 23:54:06 -0300

    Update - errores
    Retomar para corregir errores

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+const productos = [
+    {
+    nombre: "Zapatilla1",
+    precio: 10.000,
+    id: idZapatilla1,
+    stock: 10,
+    },
+    {
+        nombre: "Zapatilla2",
+        precio: 12.000,
+        id: idZapatilla2,
+        stock: 15,
+    }
+]
+productos.push(nombre:"Zapatilla3", precio:11.000, id:idZapatilla3, stock:5)
+console.log(productos)
+const suma = (parseFloat(valor1,valor2,valor3)) => {return valor1 + valor2 + valor3}
+const descuento = (suma/0.15)
+
 let idZapatilla1 = prompt("Ingrese el Id de la zapatilla que desea")
 if (idZapatilla1 <= 3) {
     let precio1 = prompt("Ingrese el importe de la zapatilla")
@@ -8,8 +27,8 @@ if (idZapatilla1 <= 3) {
             let idZapatilla2 = prompt("Ingrese el Id de la zapatilla que desea");
             if (idZapatilla2 <= 3) {
                 let precio2 = prompt("Ingrese el importe de la zapatilla");
-                let operacion = parseInt(precio1) + parseInt(precio2);
-                alert("Su total es de "+ operacion + " pesos por los productos " + idZapatilla1 + " y "+ idZapatilla2);
+                let operacion = suma;
+                alert("Su total es de "+ suma + " pesos por los productos " + idZapatilla1 + " y "+ idZapatilla2);
                     let codigoDescuento
                     do{
                         codigoDescuento = prompt("¿Posee algún código de descuento?");
@@ -18,12 +37,12 @@ if (idZapatilla1 <= 3) {
                             if (codigo != "ABCD"){
                                 prompt("Código de descuento invalido")     
                             }else{
-                                let descuento = operacion /4 
+                                let operacion = descuento
                             alert("Su valor final es de" + descuento + " pesos");
                             break
                             }
                         }else{
-                        alert("Su total es de "+ operacion + " pesos por los productos " + idZapatilla1 + " y "+ idZapatilla2);  
+                        alert("Su total es de "+ suma + " pesos por los productos " + idZapatilla1 + " y "+ idZapatilla2);  
                         }
                     }while(codigoDescuento != "no")
                     console.log(descuento)
@@ -39,5 +58,4 @@ if (idZapatilla1 <= 3) {
 else{
     prompt("Ingrese un valor entre 1 y 3")
 }
-
 // --------
